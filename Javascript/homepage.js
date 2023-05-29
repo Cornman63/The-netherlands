b8e8c6b5bccfe3ea4d59bdf2d3850bf5eb215244
Josef1470 <josef.matti05@gmail.com>
2023-05-29 21:16:07 +0200

    klar, i think

---
+++ Javascript/homepage.js
@@ -0,0 +1,12 @@
+var video = document.getElementById("Video");
+var btn = document.getElementById("Button");
+
+function myFunction() {
+  if (video.paused) {
+    video.play();
+    btn.innerHTML = "Pause";
+  } else {
+    video.pause();
+    btn.innerHTML = "Play";
+  }
+}
